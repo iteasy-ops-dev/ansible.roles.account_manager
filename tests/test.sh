cdeef13bc991fc3c17a1e2e25a7bca50061b7e7a
Ban Hun <banhun@Banui-MacBookPro.local>
2024-10-07 15:59:03 +0900

    raw test

--- tests/test.sh
+++ tests/test.sh
@@ -4,8 +4,8 @@ echo "Install Ansible Roles."
 ansible-galaxy install --force --ignore-errors -r requirements.yml -p roles
 
 echo "Test Ansible Roles."
-ansible-playbook -i inventory test.yml -e '{"action": "create", "username":"test1", "password":"1111"}'
+# ansible-playbook -i inventory test.yml -e '{"action": "create", "username":"test1", "password":"1111"}'
 # ansible-playbook -i inventory test.yml -e '{"action": "update", "username":"test1", "password":"1123"}'
-# ansible-playbook -i inventory test.yml -e '{"action": "delete", "username":"test1"}'
+ansible-playbook -i inventory test.yml -e '{"action": "delete", "username":"test1"}'
 
 
